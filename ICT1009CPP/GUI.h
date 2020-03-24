24536027507993129093e4637952142ab9716045
lim jun jack <lim_jun_jack@yahoo.com.sg>
2020-03-24 15:32:38 +0800

    Change in GUI
    Allow user to input Keywords and Number of Records

--- ICT1009CPP/GUI.h
+++ ICT1009CPP/GUI.h
@@ -1,6 +1,10 @@
 #pragma once
 #include "Crawler.h"
+#include <string>
+#include <msclr\marshal_cppstd.h>
 
+
+using namespace msclr::interop;
 //to error LNK2019: unresolved external symbols
 //due to twitcurl lib developed in older programming definitions
 //https://stackoverflow.com/questions/30412951/unresolved-external-symbol-imp-fprintf-and-imp-iob-func-sdl2
@@ -51,11 +55,18 @@ namespace CPPProject1009Gui {
 	protected:
 	private: System::Windows::Forms::TabPage^ tabPage1;
 	private: System::Windows::Forms::TabPage^ tabPage2;
-	private: System::Windows::Forms::Button^ importBtn;
+	private: System::Windows::Forms::Button^ crawlBtn;
+
 	private: System::Windows::Forms::TabPage^ tabPage3;
 	private: System::Windows::Forms::TabPage^ tabPage4;
 	private: System::Windows::Forms::TabPage^ tabPage5;
 	private: System::Windows::Forms::TabPage^ tabPage6;
+	private: System::Windows::Forms::TextBox^ recordTb;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::TextBox^ keywordTb;
+	private: System::Windows::Forms::Button^ importBtn;
+	private: System::Windows::Forms::RichTextBox^ rtbDisplay;
 
 	private:
 		/// <summary>
@@ -72,14 +83,21 @@ namespace CPPProject1009Gui {
 		{
 			this->tabControl1 = (gcnew System::Windows::Forms::TabControl());
 			this->tabPage1 = (gcnew System::Windows::Forms::TabPage());
-			this->importBtn = (gcnew System::Windows::Forms::Button());
+			this->recordTb = (gcnew System::Windows::Forms::TextBox());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->keywordTb = (gcnew System::Windows::Forms::TextBox());
+			this->crawlBtn = (gcnew System::Windows::Forms::Button());
 			this->tabPage2 = (gcnew System::Windows::Forms::TabPage());
 			this->tabPage3 = (gcnew System::Windows::Forms::TabPage());
 			this->tabPage4 = (gcnew System::Windows::Forms::TabPage());
 			this->tabPage5 = (gcnew System::Windows::Forms::TabPage());
 			this->tabPage6 = (gcnew System::Windows::Forms::TabPage());
+			this->rtbDisplay = (gcnew System::Windows::Forms::RichTextBox());
+			this->importBtn = (gcnew System::Windows::Forms::Button());
 			this->tabControl1->SuspendLayout();
 			this->tabPage1->SuspendLayout();
+			this->tabPage2->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// tabControl1
@@ -91,116 +109,218 @@ namespace CPPProject1009Gui {
 			this->tabControl1->Controls->Add(this->tabPage5);
 			this->tabControl1->Controls->Add(this->tabPage6);
 			this->tabControl1->Location = System::Drawing::Point(-1, -1);
-			this->tabControl1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->tabControl1->Name = L"tabControl1";
 			this->tabControl1->SelectedIndex = 0;
-			this->tabControl1->Size = System::Drawing::Size(861, 564);
+			this->tabControl1->Size = System::Drawing::Size(646, 458);
 			this->tabControl1->TabIndex = 0;
 			// 
 			// tabPage1
 			// 
-			this->tabPage1->Controls->Add(this->importBtn);
-			this->tabPage1->Location = System::Drawing::Point(4, 25);
-			this->tabPage1->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage1->Controls->Add(this->recordTb);
+			this->tabPage1->Controls->Add(this->label2);
+			this->tabPage1->Controls->Add(this->label1);
+			this->tabPage1->Controls->Add(this->keywordTb);
+			this->tabPage1->Controls->Add(this->crawlBtn);
+			this->tabPage1->Location = System::Drawing::Point(4, 22);
 			this->tabPage1->Name = L"tabPage1";
-			this->tabPage1->Padding = System::Windows::Forms::Padding(4, 4, 4, 4);
-			this->tabPage1->Size = System::Drawing::Size(853, 535);
+			this->tabPage1->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage1->Size = System::Drawing::Size(638, 432);
 			this->tabPage1->TabIndex = 0;
 			this->tabPage1->Text = L"Main Menu";
 			this->tabPage1->UseVisualStyleBackColor = true;
 			// 
-			// importBtn
+			// recordTb
 			// 
-			this->importBtn->Location = System::Drawing::Point(37, 42);
-			this->importBtn->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
-			this->importBtn->Name = L"importBtn";
-			this->importBtn->Size = System::Drawing::Size(101, 41);
-			this->importBtn->TabIndex = 0;
-			this->importBtn->Text = L"Import File";
-			this->importBtn->UseVisualStyleBackColor = true;
-			this->importBtn->Click += gcnew System::EventHandler(this, &GUI::importBtn_Click);
+			this->recordTb->Location = System::Drawing::Point(120, 122);
+			this->recordTb->Name = L"recordTb";
+			this->recordTb->Size = System::Drawing::Size(100, 20);
+			this->recordTb->TabIndex = 9;
+			this->recordTb->TextChanged += gcnew System::EventHandler(this, &GUI::recordTb_TextChanged);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(35, 125);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(79, 13);
+			this->label2->TabIndex = 8;
+			this->label2->Text = L"No. of Records";
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(66, 86);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(48, 13);
+			this->label1->TabIndex = 7;
+			this->label1->Text = L"Keyword";
+			// 
+			// keywordTb
+			// 
+			this->keywordTb->Location = System::Drawing::Point(120, 83);
+			this->keywordTb->Name = L"keywordTb";
+			this->keywordTb->Size = System::Drawing::Size(100, 20);
+			this->keywordTb->TabIndex = 6;
+			this->keywordTb->TextChanged += gcnew System::EventHandler(this, &GUI::keywordTb_TextChanged);
+			// 
+			// crawlBtn
+			// 
+			this->crawlBtn->Location = System::Drawing::Point(144, 163);
+			this->crawlBtn->Name = L"crawlBtn";
+			this->crawlBtn->Size = System::Drawing::Size(76, 33);
+			this->crawlBtn->TabIndex = 0;
+			this->crawlBtn->Text = L"Crawl ";
+			this->crawlBtn->UseVisualStyleBackColor = true;
+			this->crawlBtn->Click += gcnew System::EventHandler(this, &GUI::crawlBtn_Click);
 			// 
 			// tabPage2
 			// 
-			this->tabPage2->Location = System::Drawing::Point(4, 25);
-			this->tabPage2->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage2->Controls->Add(this->importBtn);
+			this->tabPage2->Controls->Add(this->rtbDisplay);
+			this->tabPage2->Location = System::Drawing::Point(4, 22);
 			this->tabPage2->Name = L"tabPage2";
-			this->tabPage2->Padding = System::Windows::Forms::Padding(4, 4, 4, 4);
-			this->tabPage2->Size = System::Drawing::Size(853, 535);
+			this->tabPage2->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage2->Size = System::Drawing::Size(638, 432);
 			this->tabPage2->TabIndex = 1;
 			this->tabPage2->Text = L"View Record";
 			this->tabPage2->UseVisualStyleBackColor = true;
 			// 
 			// tabPage3
 			// 
-			this->tabPage3->Location = System::Drawing::Point(4, 25);
-			this->tabPage3->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage3->Location = System::Drawing::Point(4, 22);
 			this->tabPage3->Name = L"tabPage3";
-			this->tabPage3->Padding = System::Windows::Forms::Padding(4, 4, 4, 4);
-			this->tabPage3->Size = System::Drawing::Size(853, 535);
+			this->tabPage3->Padding = System::Windows::Forms::Padding(3);
+			this->tabPage3->Size = System::Drawing::Size(638, 432);
 			this->tabPage3->TabIndex = 2;
 			this->tabPage3->Text = L"Function1";
 			this->tabPage3->UseVisualStyleBackColor = true;
 			// 
 			// tabPage4
 			// 
-			this->tabPage4->Location = System::Drawing::Point(4, 25);
-			this->tabPage4->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage4->Location = System::Drawing::Point(4, 22);
 			this->tabPage4->Name = L"tabPage4";
-			this->tabPage4->Size = System::Drawing::Size(853, 535);
+			this->tabPage4->Size = System::Drawing::Size(638, 432);
 			this->tabPage4->TabIndex = 3;
 			this->tabPage4->Text = L"Function2";
 			this->tabPage4->UseVisualStyleBackColor = true;
 			// 
 			// tabPage5
 			// 
-			this->tabPage5->Location = System::Drawing::Point(4, 25);
-			this->tabPage5->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage5->Location = System::Drawing::Point(4, 22);
 			this->tabPage5->Name = L"tabPage5";
-			this->tabPage5->Size = System::Drawing::Size(853, 535);
+			this->tabPage5->Size = System::Drawing::Size(638, 432);
 			this->tabPage5->TabIndex = 4;
 			this->tabPage5->Text = L"tabPage5";
 			this->tabPage5->UseVisualStyleBackColor = true;
 			// 
 			// tabPage6
 			// 
-			this->tabPage6->Location = System::Drawing::Point(4, 25);
-			this->tabPage6->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
+			this->tabPage6->Location = System::Drawing::Point(4, 22);
 			this->tabPage6->Name = L"tabPage6";
-			this->tabPage6->Size = System::Drawing::Size(853, 535);
+			this->tabPage6->Size = System::Drawing::Size(638, 432);
 			this->tabPage6->TabIndex = 5;
 			this->tabPage6->Text = L"tabPage6";
 			this->tabPage6->UseVisualStyleBackColor = true;
 			// 
+			// rtbDisplay
+			// 
+			this->rtbDisplay->Location = System::Drawing::Point(9, 17);
+			this->rtbDisplay->Name = L"rtbDisplay";
+			this->rtbDisplay->Size = System::Drawing::Size(608, 324);
+			this->rtbDisplay->TabIndex = 2;
+			this->rtbDisplay->Text = L"";
+			// 
+			// importBtn
+			// 
+			this->importBtn->Location = System::Drawing::Point(499, 377);
+			this->importBtn->Name = L"importBtn";
+			this->importBtn->Size = System::Drawing::Size(75, 23);
+			this->importBtn->TabIndex = 3;
+			this->importBtn->Text = L"Import CSV file";
+			this->importBtn->UseVisualStyleBackColor = true;
+			this->importBtn->Click += gcnew System::EventHandler(this, &GUI::importBtn_Click_1);
+			// 
 			// GUI
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(860, 560);
+			this->ClientSize = System::Drawing::Size(645, 455);
 			this->Controls->Add(this->tabControl1);
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
-			this->Margin = System::Windows::Forms::Padding(4, 4, 4, 4);
 			this->Name = L"GUI";
 			this->Text = L"Crawler GUI";
 			this->tabControl1->ResumeLayout(false);
 			this->tabPage1->ResumeLayout(false);
+			this->tabPage1->PerformLayout();
+			this->tabPage2->ResumeLayout(false);
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	private: System::Void importBtn_Click(System::Object^ sender, System::EventArgs^ e) 
-	{
-		string strInput1;
-		int intInput2;
+		/*BUTTONS for Crawling*/
+	private: System::Void crawlBtn_Click(System::Object^ sender, System::EventArgs^ e)
+	{	//Keywords
+		System::String^ strInput1;
+		//No. of Records
+		System::String^ intInput2;
+		int input2;
 
-		strInput1 = "Singapore";
-		intInput2 = 100;
+		if (keywordTb->Text == "")
+		{
+			MessageBox::Show("Please input Keyword to crawl");
+		}
+		else if (recordTb->Text == "")
+		{
+			MessageBox::Show("Please input Record to crawl");
+		}
+		else {
+			//Get Keyword from TexBox
+			strInput1 = keywordTb->Text;
+			std::string input1 = marshal_as<std::string>(strInput1);
 
-		Crawler twitterCrawler;
-		twitterCrawler.crawlerTwitter(strInput1, intInput2);
-		twitterCrawler.crawlerJsonToFile();
+			//Get Records from TexBox
+			intInput2 = recordTb->Text;
+			input2 = Convert::ToInt32(intInput2);
 
-		MessageBox::Show("Saved to File");
+
+			Crawler twitterCrawler;
+			twitterCrawler.crawlerTwitter(input1, input2);
+			twitterCrawler.crawlerJsonToFile();
+
+			MessageBox::Show("Successfully Crawled and Saved to File");
+		}
 	}
+	/*END  OF BUTTONS for Crawling*/
+
+
+	/*BUTTONS for Importing CSV File*/
+	private: System::Void importBtn_Click_1(System::Object^ sender, System::EventArgs^ e)
+	{
+		System::String^ filename;
+
+		OpenFileDialog^ openFileDialog1 = gcnew OpenFileDialog;
+
+		openFileDialog1->InitialDirectory = "c:\\";
+		//openFileDialog1->Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+		//openFileDialog1->FilterIndex = 2;
+		openFileDialog1->RestoreDirectory = true;
+
+		if (openFileDialog1->ShowDialog() == System::Windows::Forms::DialogResult::OK)
+		{
+
+
+			filename = openFileDialog1->FileName;
+			//rtbDisplay.Text = filename;
+			MessageBox::Show(filename, "Path:");
+		}
+	}
+	/*END  OF BUTTONS for Importing CSV File*/
+private: System::Void keywordTb_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void recordTb_TextChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+private: System::Void importBtn_Click(System::Object^ sender, System::EventArgs^ e) {
+}
+	
 };
 }
