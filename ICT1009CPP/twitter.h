eae46e6d90026f8f7cc6abc75fa5b7137aa78e33
Alexthetechkia <58730404+Alexthetechkia@users.noreply.github.com>
2020-03-31 15:53:32 +0800

    deepCleanText

--- ICT1009CPP/twitter.h
+++ ICT1009CPP/twitter.h
@@ -25,7 +25,7 @@ protected:
 	string filepath;
 	string t_keyword;
 public:
-	
+
 	twitterData() { dateTime = ""; userid = ""; post = ""; }
 	twitterData(string new_dateTime, string new_userid, string new_post);
 	void storeData(string new_dateTime, string new_userid, string new_post);// USED for both CNA Twitter
@@ -34,16 +34,16 @@ public:
 	string getPost() { return post; }
 
 	void set_csvfilepath(string);//for filepath from gui
-	string get_csvfilepath() {  return filepath; }
+	string get_csvfilepath() { return filepath; }
 
 };
 
 
 /*set filepath from gui*/
-void twitterData::set_csvfilepath(string path) 
+void twitterData::set_csvfilepath(string path)
 {
 	filepath = path;
-	
+
 }
 
 
@@ -145,7 +145,7 @@ void readData() {
 		temp.storeData(dateTime, userid, post);
 		twitter.push_back(temp);
 		::size++;
-		
+
 	}
 }
 //CNA Read CSV
@@ -251,16 +251,98 @@ vector<twitterData> searchKeyword(string key) {
 	return filtered;
 }
 
+// ------------------------------------- recursive function to find and erase substring -------------------------------- //
+vector<string> eraseSubstring(vector<string> commonWord, vector<string> vstrings)
+{
+	//loop through all words
+	for (int j = 0; j < vstrings.size(); j++)
+	{
+		//loop through commonWord.txt file to match common words
+		for (int k = 0; k < commonWord.size(); k++)
+		{
+			if (vstrings[j] == commonWord[k]) {
+				//get index found it std::vector<string>::iterator
+				vector<string>::iterator foundIndex = find(vstrings.begin(), vstrings.end(), vstrings[j]);
+				//get index found in int
+				int indexX = distance(vstrings.begin(), find(vstrings.begin(), vstrings.end(), vstrings[j]));
+				//do not erese if out of range
+				if (indexX < vstrings.size()) {
+					vstrings.erase(foundIndex);
+					return eraseSubstring(commonWord, vstrings);
+				}
+			}
+		}
+	}
+	return commonWord, vstrings;
+}
+
+// ----------------------------------------------- Twitter Clean Text ----------------------------------------------- //
+string deepCleanText(string strText) {
+	// convert string to lower case
+	for_each(strText.begin(), strText.end(), [](char& c) {
+		c = ::tolower(c);
+		});
+
+	// Remove punctuation
+	replace_if(strText.begin(), strText.end(),
+		[](const char& c) { return std::ispunct(c); }, ' ');
+
+	regex regexAllowAlphabetsOnly("[^a-zA-Z\\s:]");
+	regex regexMultiSpace("[\\s]+");
+	regex regexLeadSpaces("^\\s+");
+	regex regexTrailSpaces("\\s+$");
+
+	strText = regex_replace(strText, regexAllowAlphabetsOnly, "");
+	strText = regex_replace(strText, regexMultiSpace, " ");
+	strText = regex_replace(strText, regexLeadSpaces, "");
+	strText = regex_replace(strText, regexTrailSpaces, "");
+
+	//load commonWords from .txt file to vector
+	string tmpWord;
+	vector<string> commonWord;
+
+	//read common word from txt file
+	ifstream f("commonWords.txt");
+	while (f.peek() != EOF) {
+		getline(f, tmpWord, '\n');
+		//add to vector
+		commonWord.push_back(tmpWord);
+	}
+
+	//convert string to vector string
+	stringstream ss(strText);
+	istream_iterator<string> begin(ss);
+	istream_iterator<string> end;
+	vector<string> vstrings(begin, end);
+
+	//call recursive function to erase substring
+	//cuz vector size changes after every erase, to prevent out of range
+	commonWord, vstrings = eraseSubstring(commonWord, vstrings);
+
+	//store back to strText
+	strText = "";
+	for (vector<string>::const_iterator i = vstrings.begin(); i != vstrings.end(); ++i)
+		strText += *i + " ";
+
+	cout << strText << endl << endl;
+	return strText;
+}
+
+
 // ----------------------------------------------- Twitter TOP 10 COMMON WORDS USED ----------------------------------------------- //
 vector<pair<string, int>> top10WordTopics() {
 	string line;
 	string intermediate;
 	vector<string> words;
 	word_count_list word_count;
+
+	//loop all records of tweets
 	for (int i = 0; i < ::size; i++) {
-		line = twitter[i].getPost();
+		//clean text and stored into line
+		line = deepCleanText(twitter[i].getPost());
 		stringstream check1(line);
 		while (getline(check1, intermediate, ' ')) {
+			//if NOT space, push_back to words
 			words.push_back(intermediate);
 		}
 	}
@@ -269,7 +351,7 @@ vector<pair<string, int>> top10WordTopics() {
 		line = words[i];
 		// Remove punctuation.
 		int index;
-		while ((index = line.find_first_of(".,!?\\;-*+")) != string::npos)
+		while ((index = line.find_first_of("\".:,!?\\;-*+")) != string::npos)
 			line.erase(index, 1);
 
 		++word_count[line];
@@ -282,8 +364,5 @@ vector<pair<string, int>> top10WordTopics() {
 	sort(wordvector.begin(), wordvector.end(), val_lt);
 	for (int i = 0; i < 10; i++)
 		wordvector10.push_back(wordvector[i]);
-	return wordvector10; // return key value pair of limit 10 
+	return wordvector10; // return key value pair of limit 10
 }
-
-
-
